c991936abe8808f2bc80f08cfae0230220cbf8ab
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-04-24 06:16:20 +0600

    first commit

---
+++ src/app/models/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ModelsPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ModelsPage;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className='border-2 border-amber-700'>
+            
+        </div>
+    );
+};
+
+export default Navbar;
